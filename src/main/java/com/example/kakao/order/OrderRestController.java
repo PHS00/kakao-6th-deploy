42fba48732df0b7dba1f4c57c785bf8875400b15
PHS00 <hyunsoo159487@naver.com>
2023-08-01 13:35:43 +0900

    6주차과제 코드 작성

--- src/main/java/com/example/kakao/order/OrderRestController.java
+++ src/main/java/com/example/kakao/order/OrderRestController.java
@@ -1,11 +1,14 @@
 package com.example.kakao.order;
 
+import com.example.kakao._core.security.CustomUserDetails;
 import com.example.kakao._core.utils.ApiUtils;
 import com.example.kakao.order.item.Item;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -14,17 +17,26 @@ import java.util.List;
 @RestController
 public class OrderRestController {
 
+    private final OrderService orderService;
+
 
     // (기능9) 결재하기 - (주문 인서트) POST
     // /orders/save
-    public void save() {
+    // TODO : 결제하기 기능 구현 - 완료
+    @PostMapping("/orders/save")
+    public ResponseEntity<?> save(@AuthenticationPrincipal CustomUserDetails userDetails) {
+        OrderResponse.SaveDTO responseDTO = orderService.save(userDetails.getUser());
+        return ResponseEntity.ok(ApiUtils.success(responseDTO));
 
     }
 
     // (기능10) 주문 결과 확인 GET
     // /orders/{id}
-    public void findById() {
-
+    // TODO : 주문결과 확인 기능구현 - 완료
+    @GetMapping("/orders/{id}")
+    public ResponseEntity<?> findById(@PathVariable int id, @AuthenticationPrincipal CustomUserDetails userDetails) {
+        OrderResponse.FindByIdDTO responseDTO = orderService.findById(id, userDetails.getUser());
+        return ResponseEntity.ok().body(ApiUtils.success(responseDTO));
     }
 
 }
